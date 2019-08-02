a1e719272d0507023b64774c911b1f3452430db9
xwtflydream <xwtflydream@163.com>
2019-08-02 18:11:45 +0800

    添加swagger2markup Demo

---
+++ swagger2markup/src/main/java/com/xwt/swagger2markup/Swagger2markupApplication.java
@@ -0,0 +1,13 @@
+package com.xwt.swagger2markup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2markupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2markupApplication.class, args);
+    }
+
+}
---
+++ swagger2markup/src/test/java/com/xwt/swagger2markup/Swagger2markupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xwt.swagger2markup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Swagger2markupApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
